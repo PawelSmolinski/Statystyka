a1b128cae1c7968920e49a3a8074f081f2b04eff
PawelSmolinski <pawel.smolinski@phdstud.ug.edu.pl>
2024-03-24 09:42:36 +0100

    update

--- Zadania/Zadanie II.R
+++ Zadania/Zadanie II.R
@@ -1,4 +1,4 @@
-# Nowe dane, dane_terapia_kulturowa, zawierają kolumny: ID, Wiek, Grupa, Rasizm_Przed, Rasizm_Po. 
+# Nowe dane, dane_terapia, zawierają kolumny: ID, Wiek, Grupa, Objawy_Przed, Objawy_Po. 
 
 
 # Zadanie 1: Filtracja
@@ -6,25 +6,25 @@
 
 
 # Zadanie 2: Selekcja i Filtracja
-# Zadanie: Wybierz kolumny Wiek, Grupa i Rasizm_Po, a następnie wyfiltruj, aby pokazać tylko wyniki uczestników w wieku powyżej 30 lat.
+# Zadanie: Wybierz kolumny Wiek, Grupa i Objawy_Po, a następnie wyfiltruj, aby pokazać tylko wyniki uczestników w wieku powyżej 30 lat.
 
 
 # Zadanie 3: Mutacja
-# Zadanie: Dodaj nową kolumnę Zmiana_Rasizmu, która będzie różnicą między Rasizm_Przed a Rasizm_Po.
+# Zadanie: Dodaj nową kolumnę Zmiana_Objawy, która będzie różnicą między Objawy_Przed a Objawy_Po.
 
 
 # Zadanie 4: Porównanie Efektów Terapii
-# Zadanie: Oblicz średnią zmianę w poziomie rasizmu dla grupy kontrolnej, TerapiaA i TerapiaB, a następnie porównaj te wyniki
+# Zadanie: Oblicz średnią zmianę w poziomie Objawy dla grupy kontrolnej, TerapiaA i TerapiaB, a następnie porównaj te wyniki
 
 
 # Zadanie 5: Zaawansowana Filtracja
-# Zadanie: Wyfiltruj dane, aby znaleźć uczestników, którzy zanotowali spadek poziomu rasizmu (czyli Rasizm_Przed > Rasizm_Po) i są w wieku powyżej 30 lat.
+# Zadanie: Wyfiltruj dane, aby znaleźć uczestników, którzy zanotowali spadek poziomu Objawyu (czyli Objawy_Przed > Objawy_Po) i są w wieku powyżej 30 lat.
 
 
 # Zadanie 6: Zmiana nazwy zmiennych 
-# Zmień nazyw zmiennych, tak aby "Rasizm_Przed" i "Rasizm_Po" nazywały się po prostu "przed" i "po"
+# Zmień nazyw zmiennych, tak aby "Objawy_Przed" i "Objawy_Po" nazywały się po prostu "przed" i "po"
 
 
 # Zadanie 7: Zadanie 6: Przekształcenie Danych
-# Zadanie: Przekształć dane do długiego formatu, gdzie każdy wiersz przedstawia albo Rasizm_Przed, albo Rasizm_Po dla danego uczestnika.
+# Zadanie: Przekształć dane do długiego formatu, gdzie każdy wiersz przedstawia albo Objawy_Przed, albo Objawy_Po dla danego uczestnika.
 
